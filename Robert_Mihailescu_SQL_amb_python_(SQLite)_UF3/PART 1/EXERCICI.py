7a6e3d3f97518e3e43d48162ce62501e17e74c6f
rmihailescu22 <rmihailescu22@ilg.cat>
2023-05-16 16:34:04 +0200

    first commit

---
+++ Robert_Mihailescu_SQL_amb_python_(SQLite)_UF3/PART 1/EXERCICI.py
@@ -0,0 +1,32 @@
+
+import sqlite3
+import os
+ 
+def check_db(filename):
+    return os.path.exists(filename)
+ 
+db_file = 'database.db'
+schema_file = 'schema.sql'
+ 
+if check_db(db_file):
+    print('Database already exists. Exiting...')
+    exit(0)
+ 
+with open(schema_file, 'r') as rf:
+    # Read the schema from the file
+    schema = rf.read()
+ 
+with sqlite3.connect(db_file) as conn:
+    print('Created the connection!')
+    # Execute the SQL query to create the table
+    conn.executescript(schema)
+    print('Created the Table! Now inserting')
+    conn.executescript("""
+                       insert into images (name, size, date)
+                       values
+                       ('sample.png', 100, '2019-10-10'),
+                       ('ask_python.png', 450, '2019-05-02'),
+                       ('class_room.jpeg', 1200, '2018-04-07');
+                       """)
+    print('Inserted values into the table!')
+print('Closed the connection!')
